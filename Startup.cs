54f7697bd82c287120c6e6e17302fb5a08ca0e83
Brian Ragazzi <brianpragazzi@gmail.com>
2022-02-11 12:20:23 -0600

    Update message

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace dotnetapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello from TAP!");
                 });
             });
         }
